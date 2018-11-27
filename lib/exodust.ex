67cf8e12b1c2516a074cf514fbf423ea10f13247
Adam Rutkowski <hq@mtod.org>
2018-11-27 22:15:20 +0100

    Bootstrap nerves

---
+++ lib/exodust.ex
@@ -0,0 +1,18 @@
+defmodule Exodust do
+  @moduledoc """
+  Documentation for Exodust.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exodust.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exodust_test.exs
@@ -0,0 +1,8 @@
+defmodule ExodustTest do
+  use ExUnit.Case
+  doctest Exodust
+
+  test "greets the world" do
+    assert Exodust.hello() == :world
+  end
+end
